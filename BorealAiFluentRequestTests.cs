e00fa0df774ee03feedba5c68fc8613127c5d959
Olenka <132197664+olenkaxyz@users.noreply.github.com>
2023-04-30 11:57:05 +0200

    Add files via upload

---
+++ BorealAiFluentRequestTests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BorealAI.Client.Tests
+{
+    [TestClass]
+    public class BorealAiFluentRequestTests
+    {
+        [TestMethod]
+        public void CanGenerateRequestFromIntentName()
+        {
+            var intent = "test.intent";
+            var request = BorealAiRequestFactory.FromIntent(intent);
+
+            Assert.IsNotNull(request);
+            Assert.AreEqual(intent, request.GetRequestBody().IntentName);
+        }
+
+        [TestMethod]
+        public void CanGenerateRequestWithEntities()
+        {
+            var intent = "test.intent";
+            var entityAName = "entityAName";
+            var entityAValue = "entityAValue";
+            var request = BorealAiRequestFactory
+                .FromIntent(intent)
+                .WithEntity(entityAName, entityAValue);
+
+            Assert.IsNotNull(request);
+            Assert.AreEqual(intent, request.GetRequestBody().IntentName);
+            Assert.AreEqual(entityAName, request.GetRequestBody().Entities.First().EntityName);
+            Assert.AreEqual(entityAValue, request.GetRequestBody().Entities.First().EntityValue);
+        }
+    }
+}
---
+++ BorealAiRequestGreetingsTests.cs
@@ -0,0 +1,65 @@
+﻿using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BorealAI.Client.Tests
+{
+    [TestClass]
+    public class BorealAiRequestGreetingsTests
+    {
+        [TestMethod]
+        public void CanCreateGreetingRequest()
+        {
+            var request = BorealAiRequestFactory.Greetings;
+            Assert.IsNotNull(request);
+            Assert.AreEqual(BorealAiConsts.GreetingsIntent, request.GetRequestBody().IntentName);
+        }
+
+        [TestMethod]
+        public void CanCreateGreetingRequestWithUserName()
+        {
+            var request = BorealAiRequestFactory.Greetings.WithUserName("Bob");
+            Assert.IsNotNull(request);
+            Assert.AreEqual(BorealAiConsts.GreetingsIntent, request.GetRequestBody().IntentName);
+            Assert.IsTrue(request.GetRequestBody().Entities.Count == 1);
+            var entity = request.GetRequestBody().Entities.First();
+
+            Assert.IsTrue(entity.EntityName == BorealAiConsts.GreetingsEntityUserName);
+            Assert.IsTrue(entity.EntityValue == "Bob");
+        }
+
+        [TestMethod]
+        public void CallingWithUserNameMultipleTimesUsesOnlyLastName()
+        {
+            var request = BorealAiRequestFactory
+                .Greetings
+                .WithUserName("Bob")
+                .WithUserName("Rob");
+            Assert.IsNotNull(request);
+            Assert.AreEqual(BorealAiConsts.GreetingsIntent, request.GetRequestBody().IntentName);
+            Assert.IsTrue(request.GetRequestBody().Entities.Count == 1);
+            var entity = request.GetRequestBody().Entities.First();
+
+            Assert.IsTrue(entity.EntityName == BorealAiConsts.GreetingsEntityUserName);
+            Assert.IsTrue(entity.EntityValue == "Rob");
+        }
+
+        [TestMethod]
+        public void CanAddContext()
+        {
+            var request = BorealAiRequestFactory
+                .Greetings
+                .WithContext("testContextName", "testContextValue1")
+                .WithOptionalContext("optionalContext", "textContextValue3");
+
+            var body = request.GetRequestBody();
+
+            Assert.IsNotNull(request);
+            Assert.IsNotNull(body);
+            Assert.AreEqual(2, body.ContextData.Count);
+
+            Assert.IsTrue(body.ContextData.Any(context => context.ContextName == "testContextName"));
+            Assert.IsTrue(body.ContextData.Any(context => context.IsOptional));
+            Assert.IsTrue(body.ContextData.Any(context => context.IsOptional == false));
+        }
+    }
+}
